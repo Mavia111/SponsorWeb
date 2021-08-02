8ef7485718159d1ed6484a0d775a3c4240738e5f
unknown <mavia21701@gmail.com>
2021-08-02 22:26:52 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sponsor Web</title>
+    <link href="bootstrap/css/bootstrap-grid.css" rel="stylesheet" type="text/css"/>
+    <linl href="bootstrap/css/bootstrap-grid.min.css" rel="stylesheet" type="text/css"/>
+    <link href="bootstrap/css/bootstrap-grid.rtl.css" rel="stylesheet" type="text/css"/>
+    <link href="bootstrap/css/bootstrap-grid.rtl.min.css" rel="stylesheet" type="text/css"/>
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+    <link href="bootstrap/css/bootstrap-reboot.css" rel="stylesheet" type="text/css"/>
+    <link href="bootstrap/css/bootstrap-reboot.min.css" rel="stylesheet" type="text/css"/>
+    <link href="bootstrap/css/bootstrap-reboot.rtl.css" rel="stylesheet" type="text/css"/>
+    <link href="bootstrap/css/bootstrap-grid.rtl.min.css" rel="stylesheet" type="text/css"/>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="bootstrap/css/bootstrap-reboot.rtl.min.css" rel="stylesheet" type="text/css"/>
+    <link href="bootstrap/css/bootstrap-utilities.css" rel="stylesheet" type="text/css"/>
+    
+</head>
+<body>
+<header>
+		
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+  <div class="container-fluid">
+      <div class="row">
+          <div class="col-md-8">
+    <a class="navbar-brand" href="#">
+      <img src="img/logo.png" alt="" width="50" height="55" class="d-inline-block align-text-top">
+      Sponsor Web
+    </a>
+              </div>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+      <div class="navbar-nav ">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
+</header>
+    
+    <script src="bootstrap/js/bootstrap.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <srcipt src="bootstrap/js/bootstrap.esm.min.js"></srcipt>
+    <srcipt src="bootstrap/js/bootstrap.esm.js"></srcipt>
+    <srcipt src="bootstrap/js/bootstrap.bundle.js"></srcipt>
+    <srcipt src="bootstrap/js/bootstrap.bundle.min.js"></srcipt>
+</body>
+</html>
